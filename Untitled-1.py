6f8f4201acf1f248645e9785d1c1715d6ce11da7
ummerinu <81008770+ummerinu@users.noreply.github.com>
2022-02-05 14:36:29 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+
+name = input()
+print("Privit,Sergei")
+age = int(input("Skilky tobi rokiv: "))
+print(age,"- ce kruto!")
---
+++ proect.py
@@ -0,0 +1,29 @@
+m = ["kak zovut????","skolko let???","kak dela???","chem zanimaeshia???","ti bot???","kogda nachalas 2 mirovaya woina???"]
+import random
+r = random.randint(0,len(m)-1)
+rr = random.randint(0,len(m)-1)
+rrr = random.randint(0,len(m)-1)
+rrrr = random.randint(0,len(m)-1)
+rrrrr = random.randint(0,len(m)-1)
+rrrrrr = random.randint(0,len(m)-1)
+
+
+print (m[r])
+us = input("otvet-->")
+print (m[rr])
+us = input("otvet-->")
+print (m[rrr])
+us = input("otvet-->")
+print (m[rrrr])
+us = input("otvet-->")
+print (m[rrrrr])
+us = input("otvet-->")
+print (m[rrrrrr])
+
+
+
+
+if us == ("kogda nachalas 2 mirovaya woina???"):
+ us1 = input("otvet-->")
+elif us1 != ("1939"):
+  print("haha botiha")
